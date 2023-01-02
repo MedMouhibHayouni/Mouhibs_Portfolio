fef4161135dfa666e611d329ff622d0fe4b0e474
GIGABYTE <medmouhib.hayouni@esprit.tn>
2023-01-02 12:48:11 +0100

    Mouhib Portfolio

---
+++ src/app/components/home/experience/experience.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
+
+@Component({
+  selector: 'app-experience',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.scss']
+})
+export class ExperienceComponent implements OnInit {
+
+  active = 0
+
+  constructor(
+    public analyticsService: AnalyticsService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
